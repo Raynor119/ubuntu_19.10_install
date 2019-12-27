76fdb5b407d1d18b4bb229ddc4593f2162d508d1
Raynor9029 <saibod15@hotmail.es>
2019-12-26 19:26:14 -0500

    prueba1

--- instalar_ubuntu.sh
+++ instalar_ubuntu.sh
@@ -18,7 +18,8 @@ sudo dpkg --configure -a
 sudo apt-mark hold udisks2 
 sudo apt-get install xfce4 xfce4-goodies xfce4-terminal exo-utils tigervnc-standalone-server tigervnc-common dbus-x11 --no-install-recommends -y 
 sudo apt-get clean 
-mkdir -p ~/.vnc 
+mkdir -p ~/.vnc
+wget https://raw.githubusercontent.com/Raynor119/ubuntu_19.10_install/master/xstartup -P ~/.vnc/
 wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-start -P /usr/local/bin/
 wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/XFCE4/vncserver-stop -P /usr/local/bin/
 chmod +x ~/.vnc/xstartup 
